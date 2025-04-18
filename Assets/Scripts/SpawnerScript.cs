e6a744c5848ccc6bdafec7c74efc730096633259
Lobo <gbrilobo@gmail.com>
2025-04-17 21:11:22 -0300

    commit meio do caminho note crashando

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
@@ -9,19 +10,32 @@ public class SpawnerScript : MonoBehaviour
     [SerializeField] GameObject[] enemyPrefabs;
     [SerializeField] Transform[] spawnPoints;
     [SerializeField] Transform _playerTransform;
+    [SerializeField] private GameObject _door;
     private float _spawnTimer;
-    private float _spawnFrequency = 0.9f;
-    
+    private float _spawnFrequency = 0.5f;
+    private int _count = 0;
+
     void Update()
     {
         _spawnTimer += Time.deltaTime;
+    }
 
-        if (!(_spawnTimer >= _spawnFrequency) || !_playerTransform) return;
-        var randomPos = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
-            
-        var randomEnemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
-            
-        Instantiate(randomEnemy, randomPos, Quaternion.identity);
-        _spawnTimer = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_spawnTimer >= _spawnFrequency)
+        {
+            while (_count < 10)
+            {
+                    var randomPos = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+
+                    var randomEnemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+                    Instantiate(randomEnemy, randomPos, Quaternion.identity);
+                    _spawnTimer = 0;
+                    _count++;
+                
+            }
+
+        }
     }
 }
